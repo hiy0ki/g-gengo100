96f3ff19589bca001c853b088f68b7d56001a7da
hiyoki <maha.tree.21@gmail.com>
2020-04-07 23:07:33 +0900

    p00

--- ch1.scm
+++ ch1.scm
@@ -1,2 +1,13 @@
+
+;; 00. 文字列の逆順
 (define (p00 s)
-  (string->list s))
+  (define (a lis out)
+    (if (null? lis)
+        out
+        (a (cdr lis) (cons (car lis) out))))
+  (list->string (a (string->list s) '())))
+
+;; 実行
+;(p00 "stressed")
+
+
